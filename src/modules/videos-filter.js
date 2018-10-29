2a3f230ec74d2d98e11db6af1d2e3ec3c8a26968
ZoomyMan <zoomyman@gmail.com>
2018-10-29 00:05:56 +0000

    most popular videos shown first

--- src/modules/videos-filter.js
+++ src/modules/videos-filter.js
@@ -1,10 +1,11 @@
 import intersection from 'lodash.intersection';
 import sortby from 'lodash.sortby';
+import reverse from 'lodash.reverse';
 
 const videosFilter = {
 
     filter(filter, origVideos) {
-      let filteredVideos = sortby( origVideos.filter( video => video.vote_average >= filter.rating ),  'vote_average');
+      let filteredVideos = reverse(sortby( origVideos.filter( video => video.vote_average >= filter.rating ),  'vote_average'));
       
       if ( filter.selectedGenres.length ) {
         filteredVideos = filteredVideos.filter( video => intersection(video.genre_ids, filter.selectedGenres).length );
